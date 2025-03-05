467a673923007b048351627236b0c979f04de125
田丰 <tianfeng.1012@bytedance.com>
2025-03-05 18:56:17 +0800

    init

--- app/root.tsx
+++ app/root.tsx
@@ -8,18 +8,13 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
+// import ThemeStyle from "@semi-bot/semi-theme-jianying/semi.min.css?url";
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+  // {
+  //   rel: "stylesheet",
+  //   href: ThemeStyle,
+  // },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,39 @@
+// vite.config.ts
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
+import path from 'path';
+// import semi from "@douyinfe/vite-plugin-semi";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "date-fns-tz": path.resolve(
+        __dirname,
+        "node_modules/date-fns-tz/esm/index.js"
+      ),
+    },
+  },
+  ssr: {
+    noExternal: [
+      "@douyinfe/semi-ui",
+      "@douyinfe/semi-foundation",
+      "@douyinfe/semi-illustrations",
+      "@douyinfe/semi-icons",
+      "scroll-into-view-if-needed",
+    ],
+  },
   plugins: [
     remix({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
       },
     }),
     tsconfigPaths(),
+    // semi({
+    //   theme: "@semi-bot/semi-theme-jianying",
+    // }),
   ],
 });
